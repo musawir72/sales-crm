bb1c2cb8d00784de51c8a08d84730dd4eec63f3d
Muddaser Ahmed <muddaserahmed@gmail.com>
2020-01-10 23:55:46 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,16 @@ var cors = require("cors");
 app.use(express.json({ extended: false }));
 app.use(cors());
 
+app.use(function(req, res, next) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  next();
+
+});
+
 app.use("/api/user", require("./routes/user"));
 app.use("/api/job", require("./routes/job"));
 app.use("/api/auth", require("./routes/auth"));
