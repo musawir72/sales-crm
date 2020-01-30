59bcc35475511318de4c3e836fc9713a341cf605
musawir72 <musawir72.iplex@gmail.com>
2020-01-30 09:49:43 -0500

    changes made

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 const db = require("./database/db");
 var cors = require("cors");
 
